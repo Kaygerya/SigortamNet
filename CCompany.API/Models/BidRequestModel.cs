b0070c9d57c8349ff62aff07e2223134b3c2846e
Mehmet Akın <kaygerya@hotmail.com>
2021-04-13 14:17:54 +0300

    Add files via upload

---
+++ CCompany.API/Models/BidRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CCompany.API.Models
+{
+    public class BidRequestModel
+    {
+       
+        public string IdentityNumber { get; set; }
+        public string Plate { get; set; }
+        public string LicenseSerial { get; set; }
+        public string LicenseNumber { get; set; }
+
+      
+    }
+}
---
+++ CCompany.API/Models/CompanyInfoModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CCompany.API.Models
+{
+    public  class CompanyInfoModel
+    {
+        public  string CompanyName { get; set; }
+
+        public  string CompanyLogo { get; set; }
+    }
+}
