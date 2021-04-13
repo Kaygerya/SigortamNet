c6caca2f55b1cb44033a7dfc68c713b2d6ad158c
Mehmet Akın <kaygerya@hotmail.com>
2021-04-13 14:15:27 +0300

    Add files via upload

---
+++ ACompany.API/Models/BidResultModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace ACompany.API.Models
+{
+    public class BidResultModel
+    {
+        public BidResultModel()
+        {
+            Errors = new List<string>();
+        }
+        public string IdentityNumber { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CompanyLogo { get; set; }
+
+        public string BidDescription { get; set; }
+
+        public decimal BidPrice { get; set; }
+
+        public string Plate { get; set; }
+
+        public List<string> Errors { get; set; }
+        public bool IsScuccess
+        {
+            get
+            {
+                return Errors.Count == 0;
+            }
+        }
+    }
+}
---
+++ ACompany.API/Models/CompanyInfoModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ACompany.API.Models
+{
+    public  class CompanyInfoModel
+    {
+        public  string CompanyName { get; set; }
+
+        public  string CompanyLogo { get; set; }
+    }
+}
