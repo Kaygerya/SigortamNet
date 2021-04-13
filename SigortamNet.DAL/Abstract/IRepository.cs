8aa317cd2edba8e0b7b323372a82bcf1daa8e616
Mehmet Akın <kaygerya@hotmail.com>
2021-04-13 14:19:28 +0300

    Add files via upload

---
+++ SigortamNet.DAL/Abstract/IRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using SigortamNet.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace SigortamNet.DAL.Abstract
+{
+    public interface IRepository<T> where T : class, IEntity, new()
+    {
+        T GetById(string id);
+
+        bool Update(T entity);
+
+        void Insert(T entity);
+
+        bool Delete(string id);
+
+        IEnumerable<T> Query();
+
+        IEnumerable<T> Query(Expression<Func<T, bool>> filter);
+
+    }
+}
